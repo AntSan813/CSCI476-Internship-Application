5e5fe9a85af46919556d6c405be753d5080add11
Antonio Santos <sanant813@hotmail.com>
2019-01-09 16:05:15 -0500

    add  initial framework

---
+++ Internship-Application/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Internship_Application.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
