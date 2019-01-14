41f9c6d8378b84c98d0909337e22261a4854c165
AntSan813 <sanant813@hotmail.com>
2019-01-13 20:16:03 -0500

    Refactor app to clean up & add default homepage

--- Internship-Application/Controllers/HomeController.cs
+++ Internship-Application/Controllers/HomeController.cs
@@ -10,16 +10,17 @@ namespace Internship_Application.Controllers
 {
     public class HomeController : Controller
     {
+   
         public IActionResult Index()
         {
             return View();
         }
 
-        public IActionResult Privacy()
+/*        public IActionResult Privacy()
         {
             return View();
         }
-
+*/
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
