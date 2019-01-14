2e6b1c8bd69d740d6cf71d3d9f7e72bb2f83aa76
AntSan813 <sanant813@hotmail.com>
2019-01-14 09:59:01 -0500

    add sign in page

--- Internship-Application/Controllers/HomeController.cs
+++ Internship-Application/Controllers/HomeController.cs
@@ -10,17 +10,11 @@ namespace Internship_Application.Controllers
 {
     public class HomeController : Controller
     {
-   
+
         public IActionResult Index()
         {
             return View();
         }
-
-/*        public IActionResult Privacy()
-        {
-            return View();
-        }
-*/
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
---
+++ Internship-Application/Controllers/SignIn.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Internship_Application.Models;
+
+namespace Internship_Application.Controllers
+{
+    public class SignInController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
