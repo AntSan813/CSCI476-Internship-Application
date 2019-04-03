04257460e467bc9963ada5338e1f3860bb092468
Keleigh Romine <keleigh.romine@gmail.com>
2019-04-03 19:44:14 -0400

    Identity stuff is not working. User.isinrole()

--- Internship-Application/Controllers/LandingPage_Admin.cs
+++ Internship-Application/Controllers/LandingPage_Admin.cs
@@ -5,14 +5,16 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
+
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace Internship_Application.Controllers
 {
     public class LandingPage_Admin : Controller
     {
-        [Authorize(Roles = "Admin")]
+      
         // GET: /<controller>/
+
         public IActionResult Index()
         {
             return View();
