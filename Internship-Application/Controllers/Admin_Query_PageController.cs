840e05ce7748a9951a30400779333dddab775426
antonio milton santos <sanant813@hotmail.com>
2019-04-11 17:43:37 -0400

    query by company name - functional finally

--- Internship-Application/Controllers/Admin_Query_PageController.cs
+++ Internship-Application/Controllers/Admin_Query_PageController.cs
@@ -56,6 +56,8 @@ namespace Internship_Application.Controllers
             List<Companies> companies = _context.Companies.ToList<Companies>();
             ViewBag.Companies = companies;
             ViewBag.Forms = serializedFormList;
+            ViewBag.QueryByCompanyName = 0;
+
 
             //formList = forms;
 
@@ -63,36 +65,42 @@ namespace Internship_Application.Controllers
         }
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public IActionResult QueryByCompanyName(IFormCollection formcollection)
+        //public IActionResult QueryByCompanyName(IFormCollection formcollection)
+        public IActionResult Index(IFormCollection formcollection)
         {
-            var forms = _context.Forms.Where(m => m.CompanyId == Int32.Parse(formcollection["Id"])).ToList();
-
             List<FormViewModel> serializedFormList = new List<FormViewModel>();
-            foreach (var form in forms)
+            //if statement short circuits if key is not found
+            if (formcollection.ContainsKey("CompanyName"))
             {
-                Templates template = _context.Templates.Find(form.TemplateId);
-                serializedFormList.Add(new FormViewModel
+                ViewBag.QueryByCompanyName = Int32.Parse(formcollection["CompanyId"]);
+                //TODO: refactor to have all code in another function and call that function here
+                var forms = _context.Forms.Where(m => m.CompanyId == Int32.Parse(formcollection["CompanyId"])).ToList();
+      
+                foreach (var form in forms)
                 {
-                    Id = form.Id,
-                    //Form = JsonConvert.DeserializeObject<List<FormJsonModel>>(form.Answers),
-                    //FormName = template.DisplayName,
-                    //Disclaimer = template.Disclaimer,
-                    StudentName = form.StudentName,
-                    //StudentEmail = form.StudentEmail,
-                    //FacultyEmail = form.FacultyEmail,
-                    //EmployerEmail = form.EmployerEmail,
-                    UpdatedAt = form.UpdatedAt,
-                    StatusCodesViewModel = _context.StatusCodes.Find(form.StatusCodeId),
-
-                });
+                    Templates template = _context.Templates.Find(form.TemplateId);
+                    serializedFormList.Add(new FormViewModel
+                    {
+                        Id = form.Id,
+                        //Form = JsonConvert.DeserializeObject<List<FormJsonModel>>(form.Answers),
+                        //FormName = template.DisplayName,
+                        //Disclaimer = template.Disclaimer,
+                        StudentName = form.StudentName,
+                        //StudentEmail = form.StudentEmail,
+                        //FacultyEmail = form.FacultyEmail,
+                        //EmployerEmail = form.EmployerEmail,
+                        UpdatedAt = form.UpdatedAt,
+                        StatusCodesViewModel = _context.StatusCodes.Find(form.StatusCodeId),
+                    });
+                }
 
             }
-            List<Companies> companies = _context.Companies.ToList<Companies>();
-            ViewBag.Companies = companies;
+            ViewBag.Companies = _context.Companies.ToList<Companies>();
             ViewBag.Forms = serializedFormList;
 
 
-            return View("../Admin_Query_Page/Index");
+           // return View("../Admin_Query_Page/Index");
+            return View();
         }
 
     }
